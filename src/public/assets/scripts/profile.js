7c46110ab3fc078cb422e4111c9f27fb4c466ac5
AngieB26 <166353451+AngieB26@users.noreply.github.com>
2025-11-28 19:34:19 -0500

    Fix: Create profile

---
+++ src/public/assets/scripts/profile.js
@@ -0,0 +1,7 @@
+document.querySelector(".editar").addEventListener("click", () => {
+    alert("Modo edición activado");
+});
+
+document.querySelector(".kyc").addEventListener("click", () => {
+    alert("Verificación KYC iniciada");
+});
