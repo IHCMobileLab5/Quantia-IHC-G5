a2ad4f5b3ff43151702a3f93a2302c0f128df9aa
CarlosAlb101 <matadorxdm@gmail.com>
2025-11-29 23:33:15 -0500

    feat: beginner

---
+++ src/public/assets/scripts/beginner.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startBtn = document.getElementById("startBtn");
+    if (!startBtn) return;
+
+    startBtn.addEventListener("click", () => {
+        window.location.href = "login.html";
+    });
+});
